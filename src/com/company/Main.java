aff3d347bc49a983cd85294c509776ef393a764e
Alexey <leha-antipin2001@mail.ru>
2019-10-16 15:10:20 +0300

    used her

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code her
         //<type> < name > [= <значение переменной>];
         // int long short
         // float double
